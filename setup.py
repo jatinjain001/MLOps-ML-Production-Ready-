8d9f1061b55aa5ef5725cd576aa62537d739ef89
Jatin Jain <jatinjain20022001@gmail.com>
2026-02-17 15:20:15 +0530

    updated setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Jatin",
+    author_email="jatinjain20022001@gmail.com",
+    packages=find_packages()
+)
